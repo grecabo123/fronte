5af6e19b2b22b08f0479384630307b688d3d3a79
GCodingIsLife <artamay1@gmail.com>
2024-04-05 08:46:09 +0800

    changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react'
+import { Html5QrcodeScanner, Html5QrcodeSupportedFormats } from 'html5-qrcode'
+
 
 function App() {
+
+  const [scanResult, setScan] = useState(null);
+
+  useEffect(() => {
+
+    function onScanSuccess(decodedText, decodedResult) {
+      setScan(decodedText)
+      console.log(`Code matched = ${decodedText}`, decodedResult);
+    }
+
+    const formatsToSupport = [
+      Html5QrcodeSupportedFormats.QR_CODE,
+      Html5QrcodeSupportedFormats.UPC_A,
+      Html5QrcodeSupportedFormats.UPC_E,
+      Html5QrcodeSupportedFormats.UPC_EAN_EXTENSION,
+      Html5QrcodeSupportedFormats.CODE_128,
+    ];
+
+    const html5QrcodeScanner = new Html5QrcodeScanner(
+      "reader",
+      {
+        fps: 10,
+        qrbox: { width: 400, height: 400 },
+        formatsToSupport: formatsToSupport
+      },
+      false);
+    html5QrcodeScanner.render(onScanSuccess);
+
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container-fluid'>
+      <div className="row">
+        <div id='reader'></div>
+      </div>
+      {scanResult ? "" : scanResult}
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
